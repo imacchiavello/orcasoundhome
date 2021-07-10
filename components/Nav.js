0cd8abe93b86250a660a0d1722daf52eeec5a5bb
isabella macchiavello <Isaabella@Isabellas-MacBook-Air.local>
2021-07-10 14:11:57 -0500

    Added existing features to the v3-redesing branch
    Some of the dependencies  in this package are :react bootstrap and material- ui. It includes a navbar, the home and the about page

---
+++ components/Nav.js
@@ -0,0 +1,76 @@
+import Link from 'next/link'
+import navStyles from '../styles/Nav.module.css'
+import Image from 'next/image'
+import Button from 'react-bootstrap/Button'
+import { useState } from "react";
+import orcasoundlogo from '../images/orcasoundlogo.png'
+
+export default function Nav({children}){
+
+  const [isOpen,setIsOpen] = useState(false);
+  const openMenu= ()=> setIsOpen(!isOpen);
+  return <> 
+          <header className={navStyles.header}>
+              <nav className={navStyles.navbar}>
+                  <Link href='/'>
+                   <a className={navStyles.navlogo}><Image src={orcasoundlogo} width={90} height={70}/></a>
+                  </Link>
+              <ul className={isOpen === false ? 
+                      navStyles.navmenu : navStyles.navmenu +' '+ navStyles.active}>
+                  <li className={navStyles.navitem}>
+                     <Link href='/about'>
+                       <a className={isOpen === false ? 
+                                 navStyles.navlink : navStyles.navlink+' '+navStyles.active}
+                                  onClick={openMenu}>About</a>
+                      </Link>
+                  </li>
+                  <li className={navStyles.navitem}>
+                      <Link href='/learn'>
+                        <a className={isOpen === false ? 
+                                 navStyles.navlink : navStyles.navlink+' '+navStyles.active}
+                                  onClick={openMenu}>learn</a>
+                      </Link>
+                  </li>
+                 
+                  <li className={navStyles.navitem}>
+                      <Link href='/getinvolved'>
+                        <a className={isOpen === false ? 
+                                 navStyles.navlink : navStyles.navlink+' '+navStyles.active}
+                                  onClick={openMenu}>getinvolved</a>
+                      </Link>
+                  </li>
+                
+                  <li className={navStyles.navitem}>
+                      <Link href='/listen'>
+                       <a className={isOpen === false ? 
+                                  navStyles.navlink :navStyles.navlink+' '+navStyles.active}
+                                  onClick={openMenu}>Listen</a>
+                      </Link>
+                  </li>
+                  <li className={navStyles.navitem}>
+                      <Link href='/'>
+                        <a className={isOpen === false ? 
+                                 navStyles.navlink : navStyles.navlink+' '+navStyles.active}
+                                  onClick={openMenu}>Notify me</a>
+                      </Link>
+                  </li>
+              </ul>
+              <button className={isOpen === false ? 
+                                navStyles.hamburger : navStyles.hamburger+' '+navStyles.active}
+                                  onClick={openMenu}
+                                  >
+                  <span className={navStyles.bar}></span>
+                  <span className={navStyles.bar}></span>
+                  <span className={navStyles.bar}></span>
+              </button>
+              </nav>
+          </header>
+     
+   
+   </>
+}
+
+
+
+
+  
---
+++ pages/getinvolved.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Link from 'next/link'
+export const getinvolved = () => {
+    return (
+        <>
+        <Head>
+<title>Orcasound</title>
+        </Head>
+    <body>
+    <h1>Get involved</h1>
+      <h2>orcasound</h2>
+      </body>
+        </>
+    )
+}
+
+export default getinvolved
